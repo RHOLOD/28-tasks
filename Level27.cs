5f70d8045545c526fd2bb2999b647295a002bd06
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-07-05 23:01:33 +0500

    Add files via upload

---
+++ Level27.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1Space
+{
+    class Level27
+    {
+        public static string Keymaker(int k)
+        {
+            int[] massiv = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                massiv[i] = 1;
+            }
+
+            for (int i = 2; i <= k; i++)
+            {
+                int j = k / i;
+                for (int l = 1; l <= j; l++)
+                {
+                    if (massiv[l * i - 1] == 0)
+                    {
+                        massiv[l * i - 1] = 1;
+                    }
+                    else
+                    {
+                        massiv[l * i - 1] = 0;
+                    }
+                }
+            }
+            string str = "";
+            for (int i = 0; i < k; i++)
+            {
+                str += massiv[i].ToString();
+            }
+
+            return str;
+        }
+    }
+}
